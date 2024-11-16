998e2dc09c0ba2abab5f6d2f8bb1a62881a1a81b
PinakiRay-webdev <pinaki.mongodb@gmail.com>
2024-11-16 11:38:29 +0530

    arranging file management

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import { createBrowserRouter , RouterProvider } from 'react-router-dom'
+import Login from './auth/Login'
+import Navbar from './components/Navbar/Navbar'
+import Sidebar from './components/Sidebar/Sidebar'
+import AdminDashboard from './Pages/Users/Admin/Pages/Dashboard/AdminDashboard'
+import { AddFormSlice } from './redux/slice/AddFormSlice'
+import AddUsers from './Pages/Users/Admin/Components/Add Users/AddUsers'
 const App = () => {
+
+  const endPoints = createBrowserRouter([
+    {
+      path : '/',
+      element : <><Login/></>
+    },
+    {
+      path : '/admin/dashboard',
+      element :  <>
+      <Navbar/>
+      <Sidebar/>
+      <AdminDashboard/>
+      </> 
+    }
+  ])
+
   return (
-    <div>
-      Hello
+    <div className='bg-gray-200 h-screen' >
+      <RouterProvider router={endPoints}>
+
+      </RouterProvider>
+      <AddUsers/>
     </div>
   )
 }
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,56 +1,11 @@
-import React from "react";
-import { TbLogout } from "react-icons/tb";
-import  sidebar_nav_elements from '../../utils/utils'
-import { useNavigate } from "react-router-dom";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React from 'react'
 
 const Sidebar = () => {
-
-    const navigate = useNavigate();
-
-
-
-    const logout = async () =>{
-        toast.loading('logging out...' , {theme : 'dark' , position : 'top-left'})
-        await new Promise((resolve) =>{
-            setTimeout(() => {
-                resolve()
-            }, 1000);
-        })
-
-        toast.dismiss();
-        toast.success('Loggout successfully' , {theme : 'dark' , position:'top-left'})
-        setTimeout(() => {
-            navigate('/')
-        }, 1000);
-    }
-
   return (
-    <div className="fixed w-[10vw] h-screen top-0 bg-[#ffffff]">
-      <header className="flex justify-center items-center gap-4 mt-4" >
-        <p className="bg-[#ecf39e] px-2 py-1 text-xl rounded-md font-semibold">S</p>
-        <p className="font-semibold text-xl" >Schoolify</p>
-      </header>
-
-        <main className="pl-3 mt-12">
-            {sidebar_nav_elements?.map((Element , id) =>(
-                <div key={id} className="flex items-center gap-2 my-8 cursor-pointer" >
-                    <p>{<Element.icon/>}</p>
-                    <p className="capitalize" >{Element.link}</p>
-                </div>
-            ))}
-        </main>
-
-      <footer onClick={logout} className="flex items-center gap-2 border absolute w-full bottom-0 py-4 justify-center cursor-pointer bg-[#4f772d] text-white">
-        <p>
-          <TbLogout />
-        </p>
-        <p>Logout</p>
-      </footer>
-      <ToastContainer/>
+    <div className='h-screen fixed top-0 w-[10vw] bg-white' >
+      This is sidebar
     </div>
-  );
-};
+  )
+}
 
-export default Sidebar;
+export default Sidebar
