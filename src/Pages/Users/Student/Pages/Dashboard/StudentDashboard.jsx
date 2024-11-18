62f08bad735cf0f36a5117a1706e7e9dc8d366d0
PinakiRay-webdev <pinaki.mongodb@gmail.com>
2024-11-18 17:47:45 +0530

    created dashboards for students and mentors

--- src/Pages/Users/Student/Pages/Dashboard/StudentDashboard.jsx
+++ src/Pages/Users/Student/Pages/Dashboard/StudentDashboard.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useSelector , useDispatch } from 'react-redux'
 
 const StudentDashboard = () => {
+
+  const sidebarStatus = useSelector((state) => state.sidebar.isOpen)
+
   return (
-    <div>
+    <div className={`${sidebarStatus ? "pl-[10vw]" : "pl-[5vw]"}`} >
+      <div className='px-4 py-2' >
       This is student dashboard
+      </div>
     </div>
   )
 }
