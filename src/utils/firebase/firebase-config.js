df738a377aa0e65427c8eb4e9bfdd629f50c9841
PinakiRay-webdev <pinaki.mongodb@gmail.com>
2024-12-11 10:50:27 +0530

    implementation of firebase

---
+++ src/utils/firebase/firebase-config.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAZm0gNiAyrCuctoSGxoiMNNgXXUp_3i5g",
+  authDomain: "react-school-management-app.firebaseapp.com",
+  projectId: "react-school-management-app",
+  storageBucket: "react-school-management-app.firebasestorage.app",
+  messagingSenderId: "346552954219",
+  appId: "1:346552954219:web:f3fa894b60f590cdf1b02e",
+  measurementId: "G-8KYH0DF964"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
